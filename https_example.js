03f61fed868297c92d7e83d5bc66ddede2ad9c73
Anton Mihail Lachmaniucu <antonmihail@gmail.com>
2018-10-28 18:10:52 -0400

    h

--- https_example.js
+++ https_example.js
@@ -1,3 +1,20 @@
-var https = require("https");
+const https = require("https");
 
 console.log("I did it!");
+
+const options = {
+  host: "www.example.org",
+  path: "/"
+};
+
+// called by https when the request is made.
+const callback = function(response) {
+  console.log("In response handler callback!");
+  console.log("Response: ", response);
+};
+
+console.log("I'm about to make the request!");
+
+https.request(options, callback).end();
+
+console.log("I've made the request!");
